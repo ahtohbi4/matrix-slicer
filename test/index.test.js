e2a6cbdf0a5038aaa75a669f79bfcda823e64fd3
Alexander Antonov <alexandr-post@yandex.ru>
2016-05-24 02:44:43 +0300

    tests were updated

--- test/index.test.js
+++ test/index.test.js
@@ -9,25 +9,25 @@ describe('Initialization a new instance with', () => {
     it('matrix "" throws exception', () => {
         expect(() => {
             const m = new Matrix();
-        }).to.throw(/is not correct matrix./i);
+        }).to.throw(/is not correct matrix/i);
     });
 
     it('matrix "someString" throws exception', () => {
         expect(() => {
             const m = new Matrix('someString');
-        }).to.throw(/is not correct matrix./i);
+        }).to.throw(/is not correct matrix/i);
     });
 
     it('matrix "[1, 2, 3]" throws exception', () => {
         expect(() => {
             const m = new Matrix([1, 2, 3]);
-        }).to.throw(/is not correct matrix./i);
+        }).to.throw(/is not correct matrix/i);
     });
 
     it('matrix "[[1, 2], [3]]" throws exception', () => {
         expect(() => {
             const m = new Matrix([1, 2, 3]);
-        }).to.throw(/is not correct matrix./i);
+        }).to.throw(/is not correct matrix/i);
     });
 
     it('matrix "[[1, 2], [3, 4]]" not throws exception', () => {
@@ -41,6 +41,7 @@ describe('Initialization a new instance with', () => {
 });
 
 describe('Get matrix items:', () => {
+    // Row
     describe('getRow(number)', () => {
         it('getRow() throws exception', () => {
             expect(() => {
@@ -48,7 +49,7 @@ describe('Get matrix items:', () => {
                     [1, 2],
                     [3, 4]
                 ]);
-            }).to.throw(/is not correct rows identifier./i);
+            }).to.throw(/is not correct rows index/i);
         });
 
         it('getRow(6) throws exception', () => {
@@ -60,7 +61,7 @@ describe('Get matrix items:', () => {
             }).to.throw(/Could not get "6" row from the matrix/i);
         });
 
-        it('getRow(1) returns ', () => {
+        it('getRow(1) returns row with index 1 (zero-based)', () => {
             expect(() => {
                 const m = new Matrix([
                     [1, 2],
@@ -70,8 +71,9 @@ describe('Get matrix items:', () => {
         });
     });
 
+    // Rows
     describe('getRows(begin[, end])', () => {
-        it('getRows(2) returns rows from 2nd to the end', () => {
+        it('getRows(2) returns rows beginning from index 2 (zero-based) to the last one', () => {
             const m = new Matrix([
                 [1, 2],
                 [3, 4],
@@ -82,7 +84,7 @@ describe('Get matrix items:', () => {
             expect(m.getRows(2)).to.eql([[5, 6], [7, 8]]);
         });
 
-        it('getRows(-3) returns rows from (<rows amount> + begin) to the end', () => {
+        it('getRows(-3) returns rows beginning from index=(<rows amount> + begin) to the last one', () => {
             const m = new Matrix([
                 [1, 2],
                 [3, 4],
@@ -93,7 +95,7 @@ describe('Get matrix items:', () => {
             expect(m.getRows(-3)).to.eql([[3, 4], [5, 6], [7, 8]]);
         });
 
-        it('getRows(1, 2) returns rows from 1st to 2nd', () => {
+        it('getRows(1, 2) returns rows beginning from index 1 to index 2 (zero-based)', () => {
             const m = new Matrix([
                 [1, 2],
                 [3, 4],
@@ -104,7 +106,7 @@ describe('Get matrix items:', () => {
             expect(m.getRows(1, 2)).to.eql([[3, 4], [5, 6]]);
         });
 
-        it('getRows(1, -2) returns rows from 1st to (<rows amount> + end)', () => {
+        it('getRows(1, -2) returns rows beginning from index 1 (zero-based) to index=(<rows amount> + end)', () => {
             const m = new Matrix([
                 [1, 2],
                 [3, 4],
@@ -113,6 +115,75 @@ describe('Get matrix items:', () => {
             ]);
 
             expect(m.getRows(1, -2)).to.eql([[3, 4], [5, 6]]);
+        });
+    });
+
+    // Column
+    describe('getColumn(number)', () => {
+        it('getColumn() throws exception', () => {
+            expect(() => {
+                const m = new Matrix([
+                    [1, 2],
+                    [3, 4]
+                ]);
+            }).to.throw(/is not correct columns index/i);
+        });
+
+        it('getColumn(6) throws exception', () => {
+            expect(() => {
+                const m = new Matrix([
+                    [1, 2],
+                    [3, 4]
+                ]);
+            }).to.throw(/Could not get "6" column from the matrix/i);
+        });
+
+        it('getColumn(1) returns column with index 1 (zero-based)', () => {
+            expect(() => {
+                const m = new Matrix([
+                    [1, 2],
+                    [3, 4]
+                ]);
+            }).to.eql([2, 4]);
+        });
+    });
+
+    // Columns
+    describe('getColumns(begin[, end])', () => {
+        it('getColumns(2) returns columns beginning from index 2 (zero-based) to the last one', () => {
+            const m = new Matrix([
+                [1, 2, 3, 4],
+                [5, 6, 7, 8]
+            ]);
+
+            expect(m.getColumns(2)).to.eql([[3, 7], [4, 8]]);
+        });
+
+        it('getColumns(-3) returns columns beginning from index=(<columns amount> + begin) to the last one', () => {
+            const m = new Matrix([
+                [1, 2, 3, 4],
+                [5, 6, 7, 8]
+            ]);
+
+            expect(m.getColumns(-3)).to.eql([[2, 6], [3, 7], [4, 8]]);
+        });
+
+        it('getColumns(1, 2) returns columns beginning from index 1 to index 2 (zero-based)', () => {
+            const m = new Matrix([
+                [1, 2, 3, 4],
+                [5, 6, 7, 8]
+            ]);
+
+            expect(m.getColumns(1, 2)).to.eql([[2, 6], [3, 7]]);
+        });
+
+        it('getColumns(1, -2) returns columns beginning from index 1 (zero-based) to index=(<columns amount> + end)', () => {
+            const m = new Matrix([
+                [1, 2, 3, 4],
+                [5, 6, 7, 8]
+            ]);
+
+            expect(m.getColumns(1, -2)).to.eql([[2, 6], [3, 7]]);
         });
     });
 });
