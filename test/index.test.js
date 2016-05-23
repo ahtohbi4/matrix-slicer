a60836369b0396ab24868a08a5205f07ae56e944
Alexander Antonov <alexandr-post@yandex.ru>
2016-05-24 02:17:47 +0300

    tests were updated

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,6 @@
 
 const chai = require('chai');
 const expect = chai.expect;
-chai.should();
 
 const Matrix = require('../index');
 
@@ -39,5 +38,81 @@ describe('Initialization a new instance with', () => {
             ]);
         }).to.not.throw();
     });
+});
 
+describe('Get matrix items:', () => {
+    describe('getRow(number)', () => {
+        it('getRow() throws exception', () => {
+            expect(() => {
+                const m = new Matrix([
+                    [1, 2],
+                    [3, 4]
+                ]);
+            }).to.throw(/is not correct rows identifier./i);
+        });
+
+        it('getRow(6) throws exception', () => {
+            expect(() => {
+                const m = new Matrix([
+                    [1, 2],
+                    [3, 4]
+                ]);
+            }).to.throw(/Could not get "6" row from the matrix/i);
+        });
+
+        it('getRow(1) returns ', () => {
+            expect(() => {
+                const m = new Matrix([
+                    [1, 2],
+                    [3, 4]
+                ]);
+            }).to.eql([3, 4]);
+        });
+    });
+
+    describe('getRows(begin[, end])', () => {
+        it('getRows(2) returns rows from 2nd to the end', () => {
+            const m = new Matrix([
+                [1, 2],
+                [3, 4],
+                [5, 6],
+                [7, 8]
+            ]);
+
+            expect(m.getRows(2)).to.eql([[5, 6], [7, 8]]);
+        });
+
+        it('getRows(-3) returns rows from (<rows amount> + begin) to the end', () => {
+            const m = new Matrix([
+                [1, 2],
+                [3, 4],
+                [5, 6],
+                [7, 8]
+            ]);
+
+            expect(m.getRows(-3)).to.eql([[3, 4], [5, 6], [7, 8]]);
+        });
+
+        it('getRows(1, 2) returns rows from 1st to 2nd', () => {
+            const m = new Matrix([
+                [1, 2],
+                [3, 4],
+                [5, 6],
+                [7, 8]
+            ]);
+
+            expect(m.getRows(1, 2)).to.eql([[3, 4], [5, 6]]);
+        });
+
+        it('getRows(1, -2) returns rows from 1st to (<rows amount> + end)', () => {
+            const m = new Matrix([
+                [1, 2],
+                [3, 4],
+                [5, 6],
+                [7, 8]
+            ]);
+
+            expect(m.getRows(1, -2)).to.eql([[3, 4], [5, 6]]);
+        });
+    });
 });
