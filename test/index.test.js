853235d1c12b740eadf17c333e72e9c1d25f359f
Alexander Antonov <alexandr-post@yandex.ru>
2016-05-24 01:08:21 +0300

    tests were updated

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,43 @@
+'use strict';
+
 const chai = require('chai');
+const expect = chai.expect;
 chai.should();
 
 const Matrix = require('../index');
 
-describe('Description', () => {
-    it('unit', () => {
-        const m = new Matrix([[1, 2], [3, 4]]);
-        m.matrix.should.be.eql([[1, 2], [3, 4]]);
+describe('Initialization a new instance with', () => {
+    it('matrix "" throws exception', () => {
+        expect(() => {
+            const m = new Matrix();
+        }).to.throw(/is not correct matrix./i);
     });
+
+    it('matrix "someString" throws exception', () => {
+        expect(() => {
+            const m = new Matrix('someString');
+        }).to.throw(/is not correct matrix./i);
+    });
+
+    it('matrix "[1, 2, 3]" throws exception', () => {
+        expect(() => {
+            const m = new Matrix([1, 2, 3]);
+        }).to.throw(/is not correct matrix./i);
+    });
+
+    it('matrix "[[1, 2], [3]]" throws exception', () => {
+        expect(() => {
+            const m = new Matrix([1, 2, 3]);
+        }).to.throw(/is not correct matrix./i);
+    });
+
+    it('matrix "[[1, 2], [3, 4]]" not throws exception', () => {
+        expect(() => {
+            const m = new Matrix([
+                [1, 2],
+                [3, 4]
+            ]);
+        }).to.not.throw();
+    });
+
 });
