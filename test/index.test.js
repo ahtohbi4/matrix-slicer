6bf785ebd2a3481f81ac284fae62da0613e22e47
Alexander Antonov <alexandr-post@yandex.ru>
2016-05-24 23:18:14 +0300

    tests were under refactoring

--- test/index.test.js
+++ test/index.test.js
@@ -41,148 +41,100 @@ describe('Initialization a new instance with', () => {
 });
 
 describe('Get matrix items:', () => {
-    // Row
+    // Single row by index
     describe('getRow(number)', () => {
+        const m = new Matrix([
+            [1, 2],
+            [3, 4]
+        ]);
+
         it('getRow() throws exception', () => {
             expect(() => {
-                const m = new Matrix([
-                    [1, 2],
-                    [3, 4]
-                ]);
+                m.getRow();
             }).to.throw(/is not correct rows index/i);
         });
 
         it('getRow(6) throws exception', () => {
             expect(() => {
-                const m = new Matrix([
-                    [1, 2],
-                    [3, 4]
-                ]);
+                m.getRow(6);
             }).to.throw(/Could not get "6" row from the matrix/i);
         });
 
         it('getRow(1) returns row with index 1 (zero-based)', () => {
-            expect(() => {
-                const m = new Matrix([
-                    [1, 2],
-                    [3, 4]
-                ]);
-            }).to.eql([3, 4]);
+            expect(m.getRow(1)).to.eql([3, 4]);
         });
     });
 
-    // Rows
+    // Rows by indices range
     describe('getRows(begin[, end])', () => {
-        it('getRows(2) returns rows beginning from index 2 (zero-based) to the last one', () => {
-            const m = new Matrix([
-                [1, 2],
-                [3, 4],
-                [5, 6],
-                [7, 8]
-            ]);
+        const m = new Matrix([
+            [1, 2],
+            [3, 4],
+            [5, 6],
+            [7, 8]
+        ]);
 
+        it('getRows(2) returns rows beginning from index 2 (zero-based) to the last one', () => {
             expect(m.getRows(2)).to.eql([[5, 6], [7, 8]]);
         });
 
         it('getRows(-3) returns rows beginning from index=(<rows amount> + begin) to the last one', () => {
-            const m = new Matrix([
-                [1, 2],
-                [3, 4],
-                [5, 6],
-                [7, 8]
-            ]);
-
             expect(m.getRows(-3)).to.eql([[3, 4], [5, 6], [7, 8]]);
         });
 
         it('getRows(1, 2) returns rows beginning from index 1 to index 2 (zero-based)', () => {
-            const m = new Matrix([
-                [1, 2],
-                [3, 4],
-                [5, 6],
-                [7, 8]
-            ]);
-
             expect(m.getRows(1, 2)).to.eql([[3, 4], [5, 6]]);
         });
 
         it('getRows(1, -2) returns rows beginning from index 1 (zero-based) to index=(<rows amount> + end)', () => {
-            const m = new Matrix([
-                [1, 2],
-                [3, 4],
-                [5, 6],
-                [7, 8]
-            ]);
-
             expect(m.getRows(1, -2)).to.eql([[3, 4], [5, 6]]);
         });
     });
 
-    // Column
+    // Single column by index
     describe('getColumn(number)', () => {
+        const m = new Matrix([
+            [1, 2],
+            [3, 4]
+        ]);
+
         it('getColumn() throws exception', () => {
             expect(() => {
-                const m = new Matrix([
-                    [1, 2],
-                    [3, 4]
-                ]);
+                m.getColumn();
             }).to.throw(/is not correct columns index/i);
         });
 
         it('getColumn(6) throws exception', () => {
             expect(() => {
-                const m = new Matrix([
-                    [1, 2],
-                    [3, 4]
-                ]);
+                m.getColumn(6);
             }).to.throw(/Could not get "6" column from the matrix/i);
         });
 
         it('getColumn(1) returns column with index 1 (zero-based)', () => {
-            expect(() => {
-                const m = new Matrix([
-                    [1, 2],
-                    [3, 4]
-                ]);
-            }).to.eql([2, 4]);
+            expect(m.getColumn(1)).to.eql([2, 4]);
         });
     });
 
-    // Columns
+    // Columns by indices range
     describe('getColumns(begin[, end])', () => {
-        it('getColumns(2) returns columns beginning from index 2 (zero-based) to the last one', () => {
-            const m = new Matrix([
-                [1, 2, 3, 4],
-                [5, 6, 7, 8]
-            ]);
+        const m = new Matrix([
+            [1, 2, 3, 4],
+            [5, 6, 7, 8]
+        ]);
 
+        it('getColumns(2) returns columns beginning from index 2 (zero-based) to the last one', () => {
             expect(m.getColumns(2)).to.eql([[3, 7], [4, 8]]);
         });
 
         it('getColumns(-3) returns columns beginning from index=(<columns amount> + begin) to the last one', () => {
-            const m = new Matrix([
-                [1, 2, 3, 4],
-                [5, 6, 7, 8]
-            ]);
-
             expect(m.getColumns(-3)).to.eql([[2, 6], [3, 7], [4, 8]]);
         });
 
         it('getColumns(1, 2) returns columns beginning from index 1 to index 2 (zero-based)', () => {
-            const m = new Matrix([
-                [1, 2, 3, 4],
-                [5, 6, 7, 8]
-            ]);
-
             expect(m.getColumns(1, 2)).to.eql([[2, 6], [3, 7]]);
         });
 
         it('getColumns(1, -2) returns columns beginning from index 1 (zero-based) to index=(<columns amount> + end)', () => {
-            const m = new Matrix([
-                [1, 2, 3, 4],
-                [5, 6, 7, 8]
-            ]);
-
             expect(m.getColumns(1, -2)).to.eql([[2, 6], [3, 7]]);
         });
     });
