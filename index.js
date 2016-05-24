21cde46b10395420e9389fbf3286c348aefc9644
Alexander Antonov <alexandr-post@yandex.ru>
2016-05-25 00:49:52 +0300

    methods getRow and getColumn was added

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict';
 
+/**
+ * @class
+ */
 class Matrix {
+    /**
+     * @param {array} matrix
+     */
     constructor(matrix) {
         if (Array.isArray(matrix) && matrix.every((row, i, m) => {
             return Array.isArray(row) && m[0].length === row.length;
@@ -9,6 +15,30 @@ class Matrix {
         } else {
             throw new Error(`"${matrix}" is not correct matrix.`);
         }
+    }
+
+    /**
+     * @param {number} number
+     * @returns {array}
+     */
+    getRow(number) {
+        return this.matrix[number];
+    }
+
+    /**
+     * @param {number} number
+     * @returns {array}
+     */
+    getColumn(number) {
+        let result;
+
+        if (typeof this.matrix[0][number] !== 'undefined') {
+            result = this.matrix.map((row) => {
+                return row[number];
+            });
+        }
+
+        return result;
     }
 }
 
