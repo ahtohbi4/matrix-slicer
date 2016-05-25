17680f2c90fc4108fcd73e00afd0b6adc79d7c4b
Alexander Antonov <alexandr-post@yandex.ru>
2016-05-26 01:11:58 +0300

    tests were updated

--- test/index.test.js
+++ test/index.test.js
@@ -52,7 +52,7 @@ describe('Get matrix items:', () => {
             expect(m.getRow(6)).to.equal(undefined);
         });
 
-        it('getRow(1) returns row with index 1 (zero-based)', () => {
+        it('getRow(1) returns row with zero-based index 1', () => {
             expect(m.getRow(1)).to.eql([3, 4]);
         });
     });
@@ -66,20 +66,20 @@ describe('Get matrix items:', () => {
             [7, 8]
         ]);
 
-        it('getRows(2) returns rows beginning from index 2 (zero-based) to the last one', () => {
+        it('getRows(2) returns rows beginning from zero-based index 2 to the last one', () => {
             expect(m.getRows(2)).to.eql([[5, 6], [7, 8]]);
         });
 
-        it('getRows(-3) returns rows beginning from index=(<rows amount> + begin) to the last one', () => {
+        it('getRows(-3) returns rows beginning from zero-based index=(<rows amount> + begin) to the last one', () => {
             expect(m.getRows(-3)).to.eql([[3, 4], [5, 6], [7, 8]]);
         });
 
-        it('getRows(1, 2) returns rows beginning from index 1 to index 2 (zero-based)', () => {
-            expect(m.getRows(1, 2)).to.eql([[3, 4], [5, 6]]);
+        it('getRows(1, 3) returns rows beginning from zero-based index 1 to index 2', () => {
+            expect(m.getRows(1, 3)).to.eql([[3, 4], [5, 6]]);
         });
 
-        it('getRows(1, -2) returns rows beginning from index 1 (zero-based) to index=(<rows amount> + end)', () => {
-            expect(m.getRows(1, -2)).to.eql([[3, 4], [5, 6]]);
+        it('getRows(1, -1) returns rows beginning from zero-based index 1 to index=(<rows amount> + end)', () => {
+            expect(m.getRows(1, -1)).to.eql([[3, 4], [5, 6]]);
         });
     });
 
@@ -94,7 +94,7 @@ describe('Get matrix items:', () => {
             expect(m.getColumn(6)).to.equal(undefined);
         });
 
-        it('getColumn(1) returns column with index 1 (zero-based)', () => {
+        it('getColumn(1) returns column with zero-based index 1', () => {
             expect(m.getColumn(1)).to.eql([2, 4]);
         });
     });
@@ -106,20 +106,20 @@ describe('Get matrix items:', () => {
             [5, 6, 7, 8]
         ]);
 
-        it('getColumns(2) returns columns beginning from index 2 (zero-based) to the last one', () => {
+        it('getColumns(2) returns columns beginning from zero-based index 2 to the last one', () => {
             expect(m.getColumns(2)).to.eql([[3, 7], [4, 8]]);
         });
 
-        it('getColumns(-3) returns columns beginning from index=(<columns amount> + begin) to the last one', () => {
+        it('getColumns(-3) returns columns beginning from zero-based index=(<columns amount> + begin) to the last one', () => {
             expect(m.getColumns(-3)).to.eql([[2, 6], [3, 7], [4, 8]]);
         });
 
-        it('getColumns(1, 2) returns columns beginning from index 1 to index 2 (zero-based)', () => {
-            expect(m.getColumns(1, 2)).to.eql([[2, 6], [3, 7]]);
+        it('getColumns(1, 3) returns columns beginning from zero-based index 1 to index 3', () => {
+            expect(m.getColumns(1, 3)).to.eql([[2, 6], [3, 7]]);
         });
 
-        it('getColumns(1, -2) returns columns beginning from index 1 (zero-based) to index=(<columns amount> + end)', () => {
-            expect(m.getColumns(1, -2)).to.eql([[2, 6], [3, 7]]);
+        it('getColumns(1, -1) returns columns beginning from zero-based index 1 to index=(<columns amount> + end)', () => {
+            expect(m.getColumns(1, -1)).to.eql([[2, 6], [3, 7]]);
         });
     });
 
@@ -135,11 +135,11 @@ describe('Get matrix items:', () => {
             expect(m.getDiagonalMaj(6)).to.equal(undefined);
         });
 
-        it('getDiagonalMaj(2) returns major diagonal with index 2 (zero-based)', () => {
+        it('getDiagonalMaj(2) returns major diagonal with zero-based index 2', () => {
             expect(m.getDiagonalMaj(2)).to.eql([2, 7, 12]);
         });
 
-        it('getDiagonalMaj(4) returns major diagonal with index 4 (zero-based)', () => {
+        it('getDiagonalMaj(4) returns major diagonal with zero-based index 4', () => {
             expect(m.getDiagonalMaj(4)).to.eql([5, 10]);
         });
     });
@@ -153,20 +153,20 @@ describe('Get matrix items:', () => {
             [7, 8]
         ]);
 
-        it('getDiagonalsMaj(2) returns major diagonals beginning from index 2 (zero-based) to the last one', () => {
+        it('getDiagonalsMaj(2) returns major diagonals beginning from zero-based index 2 to the last one', () => {
             expect(m.getDiagonalsMaj(2)).to.eql([[3, 6], [5, 8], [7]]);
         });
 
-        it('getDiagonalsMaj(-3) returns major diagonals beginning from index=(<diagonals amount> + begin) to the last one', () => {
+        it('getDiagonalsMaj(-3) returns major diagonals beginning from zero-based index=(<diagonals amount> + begin) to the last one', () => {
             expect(m.getDiagonalsMaj(-3)).to.eql([[3, 6], [5, 8], [7]]);
         });
 
-        it('getDiagonalsMaj(1, 2) returns major diagonals beginning from index 1 to index 2 (zero-based)', () => {
-            expect(m.getDiagonalsMaj(1, 2)).to.eql([[1, 4], [3, 6]]);
+        it('getDiagonalsMaj(1, 3) returns major diagonals beginning from zero-based index 1 to index 2', () => {
+            expect(m.getDiagonalsMaj(1, 3)).to.eql([[1, 4], [3, 6]]);
         });
 
-        it('getDiagonalsMaj(1, -2) returns major diagonals beginning from index 1 (zero-based) to index=(<diagonals amount> + end)', () => {
-            expect(m.getDiagonalsMaj(1, -2)).to.eql([[1, 4], [3, 6], [5, 8]]);
+        it('getDiagonalsMaj(1, -1) returns major diagonals beginning from zero-based index 1 to index=(<diagonals amount> + end)', () => {
+            expect(m.getDiagonalsMaj(1, -1)).to.eql([[1, 4], [3, 6], [5, 8]]);
         });
     });
 
@@ -182,7 +182,7 @@ describe('Get matrix items:', () => {
             expect(m.getDiagonalMin(6)).to.equal(undefined);
         });
 
-        it('getDiagonalMin(1) returns minor diagonal with index 1 (zero-based)', () => {
+        it('getDiagonalMin(1) returns minor diagonal with zero-based index 1', () => {
             expect(m.getDiagonalMin(1)).to.eql([3, 6]);
         });
     });
@@ -196,19 +196,19 @@ describe('Get matrix items:', () => {
             [7, 8]
         ]);
 
-        it('getDiagonalsMin(2) returns minor diagonals beginning from index 2 (zero-based) to the last one', () => {
+        it('getDiagonalsMin(2) returns minor diagonals beginning from zero-based index 2 to the last one', () => {
             expect(m.getDiagonalsMin(2)).to.eql([[4, 5], [6, 7], [8]]);
         });
 
-        it('getDiagonalsMin(-3) returns minor diagonals beginning from index=(<diagonals amount> + begin) to the last one', () => {
+        it('getDiagonalsMin(-3) returns minor diagonals beginning from zero-based index=(<diagonals amount> + begin) to the last one', () => {
             expect(m.getDiagonalsMin(-3)).to.eql([[4, 5], [6, 7], [8]]);
         });
 
-        it('getDiagonalsMin(1, 2) returns minor diagonals beginning from index 1 to index 2 (zero-based)', () => {
+        it('getDiagonalsMin(1, 2) returns minor diagonals beginning from zero-based index 1 to index 2 (zero-based)', () => {
             expect(m.getDiagonalsMin(1, 2)).to.eql([[2, 3], [4, 5]]);
         });
 
-        it('getDiagonalsMin(1, -2) returns minor diagonals beginning from index 1 (zero-based) to index=(<diagonals amount> + end)', () => {
+        it('getDiagonalsMin(1, -2) returns minor diagonals beginning from zero-based index 1 to index=(<diagonals amount> + end)', () => {
             expect(m.getDiagonalsMin(1, -2)).to.eql([[2, 3], [4, 5], [6, 7]]);
         });
     });
