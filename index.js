67e37a111ba399f6082d0e00d7791385d82cac3e
Alexander Antonov <alexandr-post@yandex.ru>
2016-05-27 00:19:56 +0300

    AMD, Node and window definition were added

--- index.js
+++ index.js
@@ -1,199 +1,214 @@
-'use strict';
-
-/**
- * @class
- */
-class Matrix {
-    /**
-     * @param {array} matrix
-     */
-    constructor(matrix) {
-        if (Array.isArray(matrix) && matrix.every((row, i, m) => {
-            return Array.isArray(row) && m[0].length === row.length;
-        })) {
-            this.matrix = matrix;
-
-            this.size = {
-                width: this.matrix[0].length,
-                height: this.matrix.length,
-                diagonal: this.matrix[0].length + this.matrix.length - 1
-            };
-        } else {
-            throw new Error(`"${matrix}" is not correct matrix.`);
-        }
-    }
+(() => {
+    'use strict';
 
     /**
-     * @param {number} number
-     * @returns {array}
+     * @class
      */
-    getRow(number) {
-        return this.matrix[number];
-    }
+    class Matrix {
+        /**
+         * @param {array} matrix
+         */
+        constructor(matrix) {
+            if (Array.isArray(matrix) && matrix.every((row, i, m) => {
+                return Array.isArray(row) && m[0].length === row.length;
+            })) {
+                this.matrix = matrix;
 
-    /**
-     * @param {number} [begin=0]
-     * @param {number} [end=<rows amount>]
-     * @returns {array}
-     */
-    getRows(begin, end) {
-        let result;
-
-        begin = begin || 0;
-        begin = (begin >= 0) ? begin : this.size.height + begin;
-
-        end = end || this.size.height;
-        end = (end >= 0) ? end : this.size.height + end;
-
-        for (let i = begin; i < end; i++) {
-            result = result || [];
-            result.push(this.getRow(i));
-        }
-
-        return result;
-    }
-
-    /**
-     * @param {number} number
-     * @returns {array}
-     */
-    getColumn(number) {
-        let result;
-
-        if (typeof this.matrix[0][number] !== 'undefined') {
-            result = this.matrix.map((row) => {
-                return row[number];
-            });
-        }
-
-        return result;
-    }
-
-    /**
-     * @param {number} [begin=0]
-     * @param {number} [end=<columns amount>]
-     * @returns {array}
-     */
-    getColumns(begin, end) {
-        let result;
-
-        begin = begin || 0;
-        begin = (begin >= 0) ? begin : this.size.width + begin;
-
-        end = end || this.size.width;
-        end = (end >= 0) ? end : this.size.width + end;
-
-        for (let i = begin; i < end; i++) {
-            result = result || [];
-            result.push(this.getColumn(i));
-        }
-
-        return result;
-    }
-
-    /**
-     * @param {number} number
-     * @returns {array}
-     */
-    getDiagonalMaj(number) {
-        let result;
-
-        if (number >= 0 && number < this.size.diagonal) {
-            result = [];
-            let i, j;
-
-            if (number < this.size.width) {
-                // Above major diagonal
-                i = this.size.width - number - 1;
-                j = 0;
+                this.size = {
+                    width: this.matrix[0].length,
+                    height: this.matrix.length,
+                    diagonal: this.matrix[0].length + this.matrix.length - 1
+                };
             } else {
-                // Below major diagonal
-                i = 0;
-                j = number - this.size.width + 1;
-            }
-
-            while (i < this.size.width && j < this.size.height) {
-                result.push(this.matrix[j][i]);
-                i++; j++;
+                throw new Error(`"${matrix}" is not correct matrix.`);
             }
         }
 
-        return result;
-    }
-
-    /**
-     * @param {number} [begin=0]
-     * @param {number} [end=<parallel diagonals amount>]
-     * @returns {array}
-     */
-    getDiagonalsMaj(begin, end) {
-        let result;
-
-        begin = begin || 0;
-        begin = (begin >= 0) ? begin : this.size.diagonal + begin;
-
-        end = end || this.size.diagonal;
-        end = (end >= 0) ? end : this.size.diagonal + end;
-
-        for (let i = begin; i < end; i++) {
-            result = result || [];
-            result.push(this.getDiagonalMaj(i));
+        /**
+         * @param {number} number
+         * @returns {array}
+         */
+        getRow(number) {
+            return this.matrix[number];
         }
 
-        return result;
-    }
+        /**
+         * @param {number} [begin=0]
+         * @param {number} [end=<rows amount>]
+         * @returns {array}
+         */
+        getRows(begin, end) {
+            let result;
 
-    /**
-     * @param {number} number
-     * @returns {array}
-     */
-    getDiagonalMin(number) {
-        let result;
+            begin = begin || 0;
+            begin = (begin >= 0) ? begin : this.size.height + begin;
 
-        if (number >= 0 && number < this.size.diagonal) {
-            result = [];
-            let i, j;
+            end = end || this.size.height;
+            end = (end >= 0) ? end : this.size.height + end;
 
-            if (number < this.size.width) {
-                // Above minor diagonal
-                i = number;
-                j = 0;
-            } else {
-                // Below minor diagonal
-                i = this.size.width - 1;
-                j = number - this.size.width + 1;
+            for (let i = begin; i < end; i++) {
+                result = result || [];
+                result.push(this.getRow(i));
             }
 
-            while (i >= 0 && j < this.size.height) {
-                result.push(this.matrix[j][i]);
-                i--; j++;
+            return result;
+        }
+
+        /**
+         * @param {number} number
+         * @returns {array}
+         */
+        getColumn(number) {
+            let result;
+
+            if (typeof this.matrix[0][number] !== 'undefined') {
+                result = this.matrix.map((row) => {
+                    return row[number];
+                });
             }
+
+            return result;
         }
 
-        return result;
-    }
+        /**
+         * @param {number} [begin=0]
+         * @param {number} [end=<columns amount>]
+         * @returns {array}
+         */
+        getColumns(begin, end) {
+            let result;
 
-    /**
-     * @param {number} [begin=0]
-     * @param {number} [end=<parallel diagonals amount>]
-     * @returns {array}
-     */
-    getDiagonalsMin(begin, end) {
-        let result;
+            begin = begin || 0;
+            begin = (begin >= 0) ? begin : this.size.width + begin;
 
-        begin = begin || 0;
-        begin = (begin >= 0) ? begin : this.size.diagonal + begin;
+            end = end || this.size.width;
+            end = (end >= 0) ? end : this.size.width + end;
 
-        end = end || this.size.diagonal;
-        end = (end >= 0) ? end : this.size.diagonal + end;
+            for (let i = begin; i < end; i++) {
+                result = result || [];
+                result.push(this.getColumn(i));
+            }
 
-        for (let i = begin; i < end; i++) {
-            result = result || [];
-            result.push(this.getDiagonalMin(i));
+            return result;
         }
 
-        return result;
-    }
-}
+        /**
+         * @param {number} number
+         * @returns {array}
+         */
+        getDiagonalMaj(number) {
+            let result;
 
-module.exports = Matrix;
+            if (number >= 0 && number < this.size.diagonal) {
+                result = [];
+                let i, j;
+
+                if (number < this.size.width) {
+                    // Above major diagonal
+                    i = this.size.width - number - 1;
+                    j = 0;
+                } else {
+                    // Below major diagonal
+                    i = 0;
+                    j = number - this.size.width + 1;
+                }
+
+                while (i < this.size.width && j < this.size.height) {
+                    result.push(this.matrix[j][i]);
+                    i++; j++;
+                }
+            }
+
+            return result;
+        }
+
+        /**
+         * @param {number} [begin=0]
+         * @param {number} [end=<parallel diagonals amount>]
+         * @returns {array}
+         */
+        getDiagonalsMaj(begin, end) {
+            let result;
+
+            begin = begin || 0;
+            begin = (begin >= 0) ? begin : this.size.diagonal + begin;
+
+            end = end || this.size.diagonal;
+            end = (end >= 0) ? end : this.size.diagonal + end;
+
+            for (let i = begin; i < end; i++) {
+                result = result || [];
+                result.push(this.getDiagonalMaj(i));
+            }
+
+            return result;
+        }
+
+        /**
+         * @param {number} number
+         * @returns {array}
+         */
+        getDiagonalMin(number) {
+            let result;
+
+            if (number >= 0 && number < this.size.diagonal) {
+                result = [];
+                let i, j;
+
+                if (number < this.size.width) {
+                    // Above minor diagonal
+                    i = number;
+                    j = 0;
+                } else {
+                    // Below minor diagonal
+                    i = this.size.width - 1;
+                    j = number - this.size.width + 1;
+                }
+
+                while (i >= 0 && j < this.size.height) {
+                    result.push(this.matrix[j][i]);
+                    i--; j++;
+                }
+            }
+
+            return result;
+        }
+
+        /**
+         * @param {number} [begin=0]
+         * @param {number} [end=<parallel diagonals amount>]
+         * @returns {array}
+         */
+        getDiagonalsMin(begin, end) {
+            let result;
+
+            begin = begin || 0;
+            begin = (begin >= 0) ? begin : this.size.diagonal + begin;
+
+            end = end || this.size.diagonal;
+            end = (end >= 0) ? end : this.size.diagonal + end;
+
+            for (let i = begin; i < end; i++) {
+                result = result || [];
+                result.push(this.getDiagonalMin(i));
+            }
+
+            return result;
+        }
+    }
+
+    if (typeof define === 'function') {
+        // Define Matrix for AMD loaders
+        define(function() {
+            return Matrix;
+        });
+
+    } else if (typeof module !== 'undefined' && module.exports) {
+        // Expose Matrix for node
+        module.exports = Matrix;
+
+    } else {
+        // Otherwise write to window
+        window.Matrix = Matrix;
+    }
+}());
